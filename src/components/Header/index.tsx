0deef48d8143b2b98fb7462c6a39b94cd3805ad5
brito <britoifpe@gmail.com>
2025-07-08 10:52:25 -0300

    feat: Add balloon effect to header
    - Updates the header component with a new design resembling a speech balloon.
    - Adds a small, rotated square element to the bottom of the header to create a 'tail' for the balloon effect.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 const Header = () => {
   return (
-    <header className="flex items-center w-full h-[calc(100vh-72px)] p-8 snap-start">
-      <div>
-        <h1 className="text-4xl font-bold">
-          Matheus Henrique <br />
-          <span className="text-2xl">Fullstack &lt;dev/&gt;</span>
-        </h1>
+    <header className="w-full h-[calc(100vh-72px)] snap-start bg-orange-200">
+      <div className="relative flex items-center h-full w-full p-8 bg-[#FDF6E4] rounded-xl shadow-sm">
+        <div>
+          <h1 className="text-4xl font-bold">
+            Matheus Henrique <br />
+            <span className="text-2xl">Fullstack &lt;dev/&gt;</span>
+          </h1>
+        </div>
+        <div className="absolute left-34 -bottom-1.5 w-4 h-4 bg-[#FDF6E4] rotate-45 rounded-sm shadow-[2px_2px_2px_rgb(0,0,0,0.1)]"></div>
       </div>
     </header>
   );
