7529b9017dade57d4b84470a9c87eb37d1e475a5
brito <britoifpe@gmail.com>
2025-07-12 23:39:14 -0300

    feat: Add custom dashed border to SectionHeader
    - Implements a custom dashed border using a linear-gradient background on a '::before' pseudo-element for better visual control.
    - Applies this new style conditionally to the SectionHeader component via the 'dashed' prop.

--- src/components/common/SectionHeader/index.tsx
+++ src/components/common/SectionHeader/index.tsx
@@ -8,15 +8,13 @@ const SectionHeader = ({
   dashed = false,
 }: SectionHeaderProps) => {
   const stickyClasses = isSticky ? 'sticky top-0 z-10' : '';
-  const onPageClasses = isOnPage
-    ? 'bg-[#FFFEFD] border-b border-[#C38A48]'
-    : 'bg-orange-200';
+  const onPageClasses = isOnPage ? 'bg-[#FFFEFD]' : 'bg-orange-200';
   const shadow = isShadow ? 'shadow-[1px_1px_2px_rgb(20,20,20,0.1)]' : '';
-  const borderType = dashed ? 'border-dashed' : '';
+  const borderType = dashed ? 'dashed-border-b' : 'border-b border-[#C38A48]';
 
   return (
     <div
-      className={`text-3xl h-18 flex items-center ${onPageClasses} ${stickyClasses} ${shadow} ${borderType} transition duration-200`}
+      className={`text-3xl h-18 flex items-center ${onPageClasses} ${stickyClasses} ${shadow} ${isOnPage && borderType} transition duration-200`}
     >
       <div className=" flex py-1 h-12 w-2 bg-orange-200 rounded-r-md">
         <div className="w-1.5 bg-[#F6B76E] rounded-r-md"></div>
